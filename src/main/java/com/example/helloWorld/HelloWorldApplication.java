7a4efa54ae52e7521685e054d2db626bd39636e2
anuj <anuj@codewalnut.com>
2022-07-28 15:10:53 +0530

    make it better

---
+++ src/main/java/com/example/helloWorld/HelloWorldApplication.java
@@ -0,0 +1,26 @@
+package com.example.helloWorld;
+
+import com.example.helloWorld.HelloWorldClass.HelloWorld;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+
+	@GetMapping
+	public HelloWorld hello() {
+		 HelloWorld helloWorld = new HelloWorld("Hello World");
+		 return  helloWorld;
+	}
+
+}
