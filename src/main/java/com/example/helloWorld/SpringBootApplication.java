bfbd10d28702304779d62a8552c439232c7c8d11
anuj <anuj@codewalnut.com>
2022-07-29 18:56:30 +0530

    sandy

--- src/main/java/com/example/helloWorld/SpringBootApplication.java
+++ src/main/java/com/example/helloWorld/SpringBootApplication.java
@@ -1,20 +1,22 @@
 package com.example.helloWorld;
-
 import com.example.helloWorld.common_class.CommonClass;
+import com.example.helloWorld.student.StudentClass;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDate;
+import java.util.List;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
 @RestController
 public class SpringBootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApplication.class, args);
     }
 
     // first task
+
     @GetMapping("/helloWorld")
     public CommonClass hello() {
         CommonClass helloWorld = new CommonClass("Hello World");
@@ -39,5 +41,21 @@ public class SpringBootApplication {
 
         return divide;
     }
+    @GetMapping(path = "student")
+    public List<StudentClass> st(){
+        return List.of(new StudentClass(1l,
+                "Anuj Bhardwaj",
+                "anuj@89.com",
+                LocalDate.of(2000, 5, 25),
+                22),
+                new StudentClass(2l,
+                        "Sandeep ",
+                        "sandy@69.com",
+                        LocalDate.of(1999, 7, 20),
+                        22)
 
+        );
+
+
+    }
 }
