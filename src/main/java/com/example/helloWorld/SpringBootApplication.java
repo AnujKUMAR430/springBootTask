037b406cd3cd8525e01f7c09574a3b5ac20dd1eb
anuj <anuj@codewalnut.com>
2022-07-29 10:48:53 +0530

    input

--- src/main/java/com/example/helloWorld/SpringBootApplication.java
+++ src/main/java/com/example/helloWorld/SpringBootApplication.java
@@ -3,8 +3,13 @@ package com.example.helloWorld;
 import com.example.helloWorld.first_task.HelloWorldClass;
 import com.example.helloWorld.second_task.ArithmeticOperationsClass;
 import org.springframework.boot.SpringApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import net.minidev.json.JSONObject;
+
 
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
@@ -28,7 +33,8 @@ public class SpringBootApplication {
         ArithmeticOperationsClass add = new ArithmeticOperationsClass(addNumber());
         return add;
     }
-    int addNumber(){
+
+    int addNumber() {
         int num1 = 5;
         int num2 = 5;
         int add = num1 + num2;
@@ -41,7 +47,8 @@ public class SpringBootApplication {
         ArithmeticOperationsClass div = new ArithmeticOperationsClass(divideNumber());
         return div;
     }
-    int divideNumber(){
+
+    int divideNumber() {
         int num1 = 10;
         int num2 = 5;
         int div = num1 / num2;
@@ -51,4 +58,32 @@ public class SpringBootApplication {
 
 
 
+    @RestController
+    public class AdditionController {
+
+        private final Addition addition;
+
+        public AdditionController(Addition addition) {
+            this.addition = addition;
+        }
+
+
+        
+        @GetMapping("/add")
+        public ResponseEntity<Object> getAddition(@RequestParam Integer a, @RequestParam Integer b) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("data", addition.sum(a, b));
+            return ResponseEntity.ok(jsonObject);
+        }
+
+        @Service
+        public class Addition {
+
+            public int sum(int num1, int num2) {
+                return num1 + num2;
+            }
+        }
+
+
+    }
 }
