cb44f1ea65ca77f224c1ec13aba0a60a951d85c6
anuj <anuj@codewalnut.com>
2022-07-29 15:24:40 +0530

    hello

--- src/main/java/com/example/helloWorld/SpringBootApplication.java
+++ src/main/java/com/example/helloWorld/SpringBootApplication.java
@@ -5,6 +5,7 @@ import com.example.helloWorld.common_class.CommonClass;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
